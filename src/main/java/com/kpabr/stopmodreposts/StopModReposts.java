905000fd315ecc365fb06d4589a445d00acf1bee
Valiec <valiec2019@gmail.com>
2015-05-25 08:50:00 -0400

    Added version checker, links, ind improved validation trigger (forgot to commit during development again).

--- src/main/java/com/kpabr/stopmodreposts/StopModReposts.java
+++ src/main/java/com/kpabr/stopmodreposts/StopModReposts.java
@@ -13,8 +13,10 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.event.ClickEvent;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.FMLCommonHandler;
@@ -26,21 +28,43 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.versioning.ArtifactVersion;
 
 @Mod(modid = StopModReposts.MODID, version = StopModReposts.VERSION, name=StopModReposts.NAME)
 public class StopModReposts
 {
     public static final String MODID = "stopmodreposts";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.0";
     public static final String NAME = "StopModReposts";
+    static int versionID = 2; //Used by version checker!
     protected static ArrayList<String> warningIDs = new ArrayList<String>();
+    
+    static StopVersionChecker versionChecker = new StopVersionChecker();
+    public static StopModReposts instance;
    
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
     	FMLCommonHandler.instance().bus().register(this);
         MinecraftForge.EVENT_BUS.register(this);
+        
+        FMLCommonHandler.instance().bus().register(versionChecker);
+        MinecraftForge.EVENT_BUS.register(versionChecker);
+        
         ClientCommandHandler.instance.registerCommand(new IgnoreCommand());
+        
+        ClientCommandHandler.instance.registerCommand(new VersionCommand());
+        
+        for(ModContainer mod : Loader.instance().getActiveModList())
+        {
+        	for(ArtifactVersion modLoaded: mod.getRequirements())
+        	{
+        		if(modLoaded.getLabel().equals("stopmodreposts"))
+        		{
+        			validateMod(mod.getModId());
+        		}
+        	}
+        }
     }
     
     public static void validateMod(String modid)
@@ -135,10 +159,20 @@ public class StopModReposts
 	        	messageline2 = messageline2+", "+mod;
 	        	}
 	        }
-	    	String messageline3 = "By distributing these mods, these sites (not you) likely violate the licenses of the mods and are harmful to the mod authors. Also, these kinds of sites are known to put malware in their downloads along with the mods. Please help modders and protect yourself from malware by downloading these mods from their official sites where they were published by their developers.";
+	        ClickEvent linkmcf = new ClickEvent(ClickEvent.Action.OPEN_URL, "http://www.minecraftforum.net");
+	        ClickEvent linksmr = new ClickEvent(ClickEvent.Action.OPEN_URL, "http://www.stopmodreposts.org");
+	        ChatStyle stylemcf = new ChatStyle().setChatClickEvent(linkmcf);
+	        ChatStyle stylesmr = new ChatStyle().setChatClickEvent(linksmr);
+	    	String messageline3 = "By distributing these mods, these sites (not you) likely violate the licenses of the mods and are harmful to the mod authors. Also, these kinds of sites are known to put malware in their downloads along with the mods. Please help modders and protect yourself from malware by downloading these mods from their official sites where they were published by their developers. To prevent this message from reappearing for a certain mod, type /smrignore followed by the modid of that mod."; 
+	    	String messageline4 = "A place very likely to have an official download is the Minecraft Forums at http://www.minecraftforum.net.";
+	    	String messageline5 = "To learn more, go to http://www.stopmodreposts.org.";
+	    	String messageline6 = "If this message does not appear to start with \"[StopModReposts]\", press T to see the whole message.";
 	    	event.player.addChatMessage(new ChatComponentText(messageline1));
 	    	event.player.addChatMessage(new ChatComponentText(messageline2));
 	    	event.player.addChatMessage(new ChatComponentText(messageline3));
+	    	event.player.addChatComponentMessage(new ChatComponentText(messageline4).setChatStyle(stylemcf));
+	    	event.player.addChatComponentMessage(new ChatComponentText(messageline5).setChatStyle(stylesmr));
+	    	event.player.addChatMessage(new ChatComponentText(messageline6));
     	}
 	    	 
     }
